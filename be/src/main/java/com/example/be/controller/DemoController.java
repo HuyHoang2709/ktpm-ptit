707cfd46c0e0539855f8c5d620569ea62215e98b
Bui Huy Hoang <buihuyhoang2709.dev@gmail.com>
2025-05-25 16:45:03 +0700

    BE: update demo api

--- be/src/main/java/com/example/be/controller/DemoController.java
+++ be/src/main/java/com/example/be/controller/DemoController.java
@@ -1,21 +1,36 @@
 package com.example.be.controller;
 
+import com.example.be.entity.GiaoVien;
+import com.example.be.repository.GiaoVienRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
 public class DemoController {
 
-    @GetMapping(value = "/test")
+    @Autowired
+    private GiaoVienRepository giaoVienRepository;
+
+    @GetMapping("/test")
     public ResponseEntity<Map<String, Object>> test() {
         Map<String, Object> map = new HashMap<>();
         map.put("message", "Server is running at port 8080");
         return ResponseEntity.ok(map);
+    }
+
+    @PostMapping("/test/giaovien")
+    public ResponseEntity<List<GiaoVien>> timGiaoVienTheoNgaySinh(@RequestParam String ngaysinh) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        List<GiaoVien> listGv = giaoVienRepository.findGiaoVienByNgaysinh(LocalDate.parse(ngaysinh, formatter));
+
+        return ResponseEntity.ok(listGv);
     }
 }
--- be/src/main/java/com/example/be/repository/GiaoVienRepository.java
+++ be/src/main/java/com/example/be/repository/GiaoVienRepository.java
@@ -3,9 +3,14 @@ package com.example.be.repository;
 import com.example.be.entity.GiaoVien;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 public interface GiaoVienRepository extends JpaRepository<GiaoVien, Integer> {
     Optional<GiaoVien> findByUsername(String username);
     Boolean existsByUsername(String username);
+
+    // For testing only
+    List<GiaoVien> findGiaoVienByNgaysinh(LocalDate ngaysinh);
 }
